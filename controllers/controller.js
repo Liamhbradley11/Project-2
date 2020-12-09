0649605528690ad37d23b7986dfd2918a142e75e
Liam Bradley <liamhbradley11@gmail.com>
2020-12-09 12:08:14 -0800

    moved config

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,101 +1,298 @@
 const express = require('express');
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
-const User = require('../models/user.js')
 const app = express();
+const db = require("../models");
+const bcrypt = require('bcryptjs');
+const path = require("path");
 
+module.exports = (app) => {
 
-module.exports =(app) => {
-
-    // home page // 
+// landing page
     app.get('/', (request, response) => {
-        response.render('index')
-    });
-
-
-    app.get('/login', (request, response) => {
-        response.render('login');
-    });
-
-    app.get('/register', (request, response) => {
-        response.render('register');
-    })
-
-    app.post('/register', (request, response) => {
-         db.User.find({where: {username: request.username}}).success((user) => {
-                if(!user) {
-                    db.User.create({
-                        username: request.body.username, 
-                        password: request.body.password, 
-                        name: request.body.name, 
-                        email: request.body.email
-                    }).error((err) => {
-                        console.log(err);
-                    });
-                } else {
-                    response.redirect('/register')
-                }
-            })
-            response.redirect('/')
-    });
-
-    app.post('/users/login',
-          passport.authenticate('local', {
-                successRedirect: '/profile',
-                failureRedirect: '/register',
-                failureFlash: true
-            }),
-        (request, response) => {
-            response.redirect('/')
-   });
-
-    app.get('/logout', (request, response) => {
-        request.logout();
-        request.flash('You are logged out');
-        requst.redirect('/login');
-    })
-    return User;
-};
-
-
-    // Form page for testing DB Entry
-    app.get('/workout', (request, response) => {
-        response.sendFile(path.join(__dirname,"../views/test-db-entry.html"));
-    });
-
-    // Form page to enter a new workout
-    app.post('/workout/new', (request, response) => {
-        console.log(request.body);
-        db.WorkoutDay.create({
-            day: request.body.day,
-            text: request.body.text,
-            ProgramId: request.body.program
-        }).then((dbWorkOut) => {
-            console.log(dbWorkOut);
+        db.Program.findAll({
+        }).then((result) =>{
+            var progObject = {
+                programs: result
+            };
+            response.render('landing', progObject);
         });
-        response.redirect('/workout');
     });
 
-
-    // Not working yet...Trying to do an update to a USERs PROGRAM
-    // page for update
-    app.get('/workout/update', (request, response) => {
-        response.sendFile(path.join(__dirname,"../views/test-update-program.html"));
+// User page
+    app.get('/user/workout', isLoggedIn, (request, response) => {
+        db.WorkoutDay.findOne({
+            where: {
+                day: request.user.currentDay,
+                ProgramId: request.user.ProgramId
+            },
+            include: {
+                model: db.Program,
+                attributes: ['name']
+            }
+        }).then((results) => {
+            var workoutObject = {
+                singleWorkout: results
+            };
+            response.render('user-workout', workoutObject);
+        });
     });
-    // route for update
-    app.put('/workout/update', (request, response) => {
-        console.log(request.body);
+// Update when user 'COMPLETES' workout -- helps progress tracking bar and getting next workout
+    app.put('/user/workout', isLoggedIn, (request, response) => {
+        db.User.update(
+            request.body,
+        {
+            where: {id: request.user.id}
+        }).then((result) => {
+            response.json(result);
+        });
+    });
+
+// Gets User information
+    app.get('/user/profile', isLoggedIn, (request, response) => {
+        db.User.findOne({
+            where: {
+                id: request.user.id
+            },
+            attributes: ['name', 'username', 'email', 'password', 'currentDay'],
+            include: {
+                model: db.Program,
+                attritbues: ['name', 'days']
+            }
+        }).then((result) => {
+            var userInfo = {
+                info: result
+            };
+            response.render('user-profile', userInfo);
+        });
+    });
+// Updates user's information
+    app.put('/user/profile', isLoggedIn, (request, response) => {
+        db.User.update({
+            name: request.body.name,
+            username: request.body.username,
+            email:request.body.email
+        },{
+            where: {id: request.user.id}
+        }).then((result) => {
+            response.redirect('/user/profile');
+        })
+    });
+
+// Client List
+    app.get('/admin/clients', (request, response) => {
+        db.User.findAll({
+            attributes: ['id', 'name', 'username', 'email', 'currentDay'],
+            include: {
+                model: db.Program,
+                attributes: ['name', 'description', 'days']
+            }
+        }).then((result) =>{
+            var lengthObject = {
+                length: result.length
+            }
+            var clientList = {
+                clients: result,
+                length: lengthObject
+            };
+            response.render('admin-client', clientList);
+        });
+    });
+
+// New program page
+    app.get('/admin/create', (request, response) => {
+        response.render('admin-create');
+    });
+
+// Creates new program
+    app.post('/admin/create', (request, response) => {
+        db.Program.create(
+            request.body
+        ).then( (dbPost) => {
+            response.json(dbPost);
+        });
+    });
+
+// Form page for NEW WORKOUT
+    app.get('/admin/create/workout', (request, response) => {
+
+        db.Program.findAll({
+        }).then((result) =>{
+            var thingy = result.length - 1;
+            var progList = {
+                programs: result[thingy]
+            };
+            response.render('admin-new-workout', progList);
+        });
+    });
+    
+// Create new workout
+    app.post('/admin/create/workout', (request, response) => {
+        db.WorkoutDay.create(request.body)
+        .then((dbWorkOut) => {
+            response.render('admin-new-workout');
+        });
+        
+    });
+
+// Form Page to UPDATE WORKOUT
+    app.get('/admin/workout/update', (request, response) =>{
+        response.render('admin/workout/update');
+    });
+
+// gets all of the programs
+    app.get('/admin/programs', (request, response) => {
+        db.Program.findAll({
+        }).then((result) =>{
+            var progObject = {
+                programs: result
+            };
+            response.render('programs', progObject);
+        });
+    });
+
+// gets the individual workout program
+    app.get('/admin/programs/:id', (request, response) => {
+        db.Program.findOne({
+            where: {
+                id: request.params.id
+            },
+            attributes: ['id', 'name'],
+            include: {
+                model: db.WorkoutDay,
+                attributes: ['day', 'text']    
+            }
+        }).then((results) =>{
+ 
+            response.json(results);
+        });
+    });
+
+// Form page for Client Program UPDATE
+    app.get('/admin/clients/update', (request, response) => {
+        db.User.findAll({   
+        }).then((result) =>{
+            var userObject = {
+                user: result
+            };
+            response.render('clientUpdate', userObject);
+        });
+    });
+    app.put('/admin/clients/update:id', (request, response) => {
         db.User.update({
             ProgramId: request.body.program
-        }, {
-            // purposefully put in the first user since handlebars is not up and running yet
-            where: {
-                id: 1
-            }
-        }).then((dbWorkOut) => {
-            console.log(dbWorkOut);
-        });
-        response.redirect('/workout/update');
+        },{
+            where: {id: request.params.id}
+        }).then(() => {
+            response.redirect('/admin/clients/update');
+        });  
     });
 
+    
+    app.post('/login', passport.authenticate('local-signIn', 
+        {  successRedirect: '/user/workout',
+        failureRedirect: '/',
+        failureFlash: true
+    }
+    ));
 
+    app.post('/login/admin', passport.authenticate('local-signIn', 
+        {  successRedirect: '/admin/clients',
+        failureRedirect: '/',
+        failureFlash: true}
+    ));
+
+    app.get('/logout', isLoggedIn, (request, response, next) => {
+        request.logout();
+        request.flash('success_msg', "You are logged out");
+        response.redirect('/');
+    })
+
+
+    // User Registration routes    
+    app.post('/users/register', (request, response) => {
+         let name = request.body.name; 
+         let username = request.body.username;
+         let email = request.body.email;
+         let password = request.body.password;
+         let password2 = request.body.password2;
+         let program = request.body.workouts;
+         
+          request.checkBody('name', 'Name is required').notEmpty();
+          request.checkBody('email', 'Email is required').notEmpty();
+          request.checkBody('email', 'Email is not valid').isEmail();
+          request.checkBody('username', 'username is required').notEmpty()
+          request.checkBody('password', 'Password is required').notEmpty()
+          request.checkBody('password2', 'Passwords do not match').equals(request.body.password);
+             
+              let errors = request.validationErrors();
+              if(errors){
+                  response.redirect('/', {errors: errors});
+              } else {
+                
+                 let salt = bcrypt.genSaltSync(10)
+                 let hashedPassword = bcrypt.hashSync(password, salt) 
+                 db.User.create({
+                     name: name,
+                     username: username,
+                     password: hashedPassword,
+                     salt: salt,
+                     email: email,
+                     ProgramId: program
+                 }).then(
+                    (user)=>{
+                       passport.authenticate("local-signIn", {failureRedirect:"/signup", successRedirect: "/user/profile"})(request, response) 
+                       request.flash('success_msg', 'You are registered and can now login');
+                     }
+             )}
+     });
+
+// ******************************************************************************
+// *************************** PASSPORT CONFIG***********************************
+// ******************************************************************************
+
+      passport.use('local-signIn', new LocalStrategy.Strategy(
+        (username, password, done) => {
+        db.User.findOne({ where: { 'username': username }}).then((user) => {
+            if(!user){return done(null, false, {message:'Unknown User'})}
+            let hashedPW = bcrypt.hashSync(password, user.salt) 
+            if(user.password === hashedPW){
+              return  done(null, user);
+            }
+            return done(null, false , { message: 'Incorrect password.'})
+          })
+        }
+      ));
+
+      // function that allowes rout access only to logged in users /// 
+      function isLoggedIn(request, response, next){
+          if(request.isAuthenticated()){
+              return next();
+          }
+          response.redirect('/');
+          
+      }
+    // function that allowes rout access only to logged in users /// 
+          function notLoggedIn(request, response, next){
+          if(!request.isAuthenticated()){
+              return next();
+          }
+          response.redirect('/');
+      }
+        // Serialize Sessions
+      passport.serializeUser((user, done) => {
+        done(null, user);
+      });
+
+    //   Deserialize Sessions
+      passport.deserializeUser((user, done) => {
+        db.User.findOne({where: {'username': user.username}}).then((user) => {
+          done(null, user);
+        }).catch((err) => {
+          done(err, null)
+        });
+      });
+
+
+
+};
--- public/javascript/user.js
+++ public/javascript/user.js
@@ -1,20 +1,42 @@
-const bcrypt = require('bcryptjs');
+// Progress bar percent
+$(document).ready(() => {
+    let currentDay = $('.progress-bar h1').attr('id');
+    let days = $('.progress-bar h1').attr('value');
+    let percent = (currentDay / days) * 100;
+    $('.progress-bar span').css('width', `${percent}%`)
+});
 
-module.exports = (sequelize, DataTypes)=> {
-  var User = sequelize.define("User", {
-    username:{type: DataTypes.STRING },
-    password:{type: DataTypes.STRING },
-    email:{type: DataTypes.STRING },
-    name:{type: DataTypes.STRING },
-    salt:{type: DataTypes.STRING},
-    currentDay:{type: DataTypes.INTEGER, defaultValue: 1}
- },{
-    classMethods: {
-      associate: function(models){
-          User.belongsTo(models.Program);
-      }
-    },
-    timestamps: false
- });
-  return User;
-};
+$('.edit-button').on('click', () => {
+    $('.user-info').hide();
+    $('.info-list input').show();
+    $('.edit-button').hide();
+    $('.save-button').show();
+})
+
+$('.save-button').on('click', () => {
+    $('.user-info').show();
+    $('.info-list input').hide();
+    $('.edit-button').show();
+    $('.save-button').hide();
+});
+
+// complete button update
+$('.workout-card').on('click', '.complete-button button', (e) => {
+    e.preventDefault();
+    let day = parseInt($(e.target).attr('id'));
+    day = day+1;
+    let update = {
+        currentDay: day
+    }
+
+    let queryUrl = '/user/workout/';
+    
+    $.ajax({
+        method: "PUT",
+        url: queryUrl,
+        data: update
+    }).done(function(){
+        window.location.href = "/user/profile";
+    }); 
+    
+})
