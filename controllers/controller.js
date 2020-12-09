0bff1b782ce7503bc77467ce41b17e8ca9ea4e6c
Liam Bradley <liamhbradley11@gmail.com>
2020-12-08 17:15:02 -0800

    forgot to save

--- controllers/controller.js
+++ controllers/controller.js
@@ -56,3 +56,46 @@ module.exports =(app) => {
     })
     return User;
 };
+
+
+    // Form page for testing DB Entry
+    app.get('/workout', (request, response) => {
+        response.sendFile(path.join(__dirname,"../views/test-db-entry.html"));
+    });
+
+    // Form page to enter a new workout
+    app.post('/workout/new', (request, response) => {
+        console.log(request.body);
+        db.WorkoutDay.create({
+            day: request.body.day,
+            text: request.body.text,
+            ProgramId: request.body.program
+        }).then((dbWorkOut) => {
+            console.log(dbWorkOut);
+        });
+        response.redirect('/workout');
+    });
+
+
+    // Not working yet...Trying to do an update to a USERs PROGRAM
+    // page for update
+    app.get('/workout/update', (request, response) => {
+        response.sendFile(path.join(__dirname,"../views/test-update-program.html"));
+    });
+    // route for update
+    app.put('/workout/update', (request, response) => {
+        console.log(request.body);
+        db.User.update({
+            ProgramId: request.body.program
+        }, {
+            // purposefully put in the first user since handlebars is not up and running yet
+            where: {
+                id: 1
+            }
+        }).then((dbWorkOut) => {
+            console.log(dbWorkOut);
+        });
+        response.redirect('/workout/update');
+    });
+
+
